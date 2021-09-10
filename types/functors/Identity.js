9c4230e61100a93c1b4ed7a35a988ffd0c118cde
Jason Barr <jason@jasonsbarr.com>
2021-09-09 22:22:50 -0500

    Add concat method to Identity functor

--- types/functors/Identity.js
+++ types/functors/Identity.js
@@ -4,6 +4,7 @@ export const Identity = (x) => ({
   chain: (fn) => fn(x),
   fold: (fn) => fn(x),
   inspect: () => `Identity(${x})`,
+  concat: (o) => Identity(x.concat(o)),
 });
 
 Identity.of = (x) => Identity(x);
