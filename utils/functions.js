844e73d887f28ab28131285237760e36d626b98b
Jason Barr <jason@jasonsbarr.com>
2021-09-09 23:03:33 -0500

    Add all, any, sum, and product functions

--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,5 @@
+import { tryCatch } from "../types/functors/Result";
+
 export const identity = (x) => x;
 
 export const noop = () => {};
@@ -31,3 +33,25 @@ export const defer =
     : typeof process !== "undefined"
     ? (f) => process.nextTick(f)
     : (f) => setTimeout(f, 0);
+
+// Returns a single array of the function args based on whether the first arg is an array
+export const getArrayFromArgs = (...args) =>
+  Array.isArray(args[0]) ? args[0] : args;
+
+// returns true if all arguments or all elements in a single array argument are true
+export const all = (...args) =>
+  tryCatch(() =>
+    getArrayFromArgs(args).reduce((acc, x) => acc && x, true)
+  ).fold(() => false, identity);
+
+// returns true if any argument or an element in a single array argument is true
+export const any = (...args) =>
+  tryCatch(() =>
+    getArrayFromArgs(args).reduce((acc, x) => acc || x, false)
+  ).fold(() => false, identity);
+
+export const sum = (...args) =>
+  getArrayFromArgs(args).reduce((acc, x) => acc + x, 0);
+
+export const product = (...args) =>
+  getArrayFromArgs(args).reduce((acc, x) => acc * x, 1);
