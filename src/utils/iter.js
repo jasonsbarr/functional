085cc738ef0e73f1609cb8b9a48c538b83747517
Jason Barr <jason@jasonsbarr.com>
2021-09-12 15:31:16 -0500

    Allow RegExp as search param in count

--- src/utils/iter.js
+++ src/utils/iter.js
@@ -54,6 +54,8 @@ export const count = (search, iter) => {
   for (let item of iter) {
     if (typeof search === "function") {
       count += search(item) ? 1 : 0;
+    } else if (search instanceof RegExp) {
+      count += search.test(item) ? 1 : 0;
     } else {
       count += equals(item, search) ? 1 : 0;
     }
