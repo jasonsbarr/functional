cbf77a0a5a9349ce10d7d43700978d928ab68b11
Jason Barr <jason@jasonsbarr.com>
2021-09-12 18:38:18 -0500

    Redefine Identity type as a class

--- src/types/monads/Identity.js
+++ src/types/monads/Identity.js
@@ -1,12 +1,51 @@
-export const Identity = (x) => ({
-  kind: "Identity",
-  value: x,
-  map: (fn) => Identity(fn(x)),
-  chain: (fn) => fn(x),
-  fold: (fn) => fn(x),
-  inspect: () => `Identity(${x})`,
-  concat: (o) => Identity(x.concat(o)),
-});
+class I {
+  constructor(value) {
+    this._value = value;
+
+    Object.defineProperty(this, "_value", {
+      configurable: false,
+      enumerable: true,
+      writable: false,
+      value: value,
+    });
+
+    Object.defineProperty(this, "kind", {
+      configurable: false,
+      enumerable: false,
+      writable: false,
+      value: "Identity",
+    });
+
+    Object.defineProperty(this, "constructor", {
+      configurable: false,
+      enumerable: false,
+      writable: false,
+      value: Identity,
+    });
+  }
+
+  map(fn) {
+    return Identity(fn(x));
+  }
+  chain(fn) {
+    return fn(x);
+  }
+  fold(fn) {
+    return fn(x);
+  }
+  inspect() {
+    return `Identity(${x})`;
+  }
+  concat(o) {
+    return Identity(x.concat(o));
+  }
+
+  toString() {
+    return this.inspect();
+  }
+}
+
+export const Identity = (x) => new I(x);
 
 Identity.of = (x) => Identity(x);
 Identity.isIdentity = (obj) => obj.kind === "Identity";
