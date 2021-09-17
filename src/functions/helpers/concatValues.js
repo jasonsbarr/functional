8f8c26b68cbf38d5a570e4f0bb1f836fe49f2b4f
Jason Barr <jason@jasonsbarr.com>
2021-09-16 23:04:27 -0500

    Implement generalized concat function

---
+++ src/functions/helpers/concatValues.js
@@ -0,0 +1,20 @@
+import { keys } from "../object/keys.js";
+import { isNil } from "./isNil.js";
+
+export const concat = (value1, value2) => {
+  if (
+    typeof value1 === "string" ||
+    typeof value1 === "number" ||
+    typeof value1 === "bigint"
+  ) {
+    value1 = value1 + value2;
+  } else if (typeof value1 === "boolean") {
+    value1 = value1 && value2;
+  } else if (isNil(value1)) {
+    value1 = value2;
+  } else if (typeof value1 === "symbol") {
+    value1 = Symbol(value1.description + value2.description);
+  }
+  // add cases for Maps and Sets?
+  return value1.concat(value2);
+};
