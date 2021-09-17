72f7d72c8f134765aa8f446fb599235f009f09c8
Jason Barr <jason@jasonsbarr.com>
2021-09-16 22:55:26 -0500

    Implement dict concat function

---
+++ src/functions/dict/concat.js
@@ -0,0 +1,29 @@
+import { isNil } from "../helpers/isNil.js";
+import { keys } from "../object/keys.js";
+import { copy } from "./copy.js";
+
+// Note that dict1 and dict2 should have the same keys and be filled
+// completely with Semigroups (types that can be concatenated).
+// Safest when both dicts are of type Dict<string, V> where V
+// is a concattable type.
+export const concat = (dict1, dict2) => {
+  let c = copy(dict1);
+  for (let key of keys(c)) {
+    if (
+      typeof c[key] === "string" ||
+      typeof c[key] === "number" ||
+      typeof c[key] === "bigint"
+    ) {
+      c[key] = c[key] + dict2[key];
+    } else if (typeof c[key] === "boolean") {
+      c[key] = c[key] && dict2[key];
+    } else if (isNil(c[key])) {
+      c[key] = dict2[key];
+    } else if (typeof c[key] === "symbol") {
+      c[key] = Symbol(c[key].description + dict2[key].description);
+    } else {
+      c[key].concat(dict2[key]);
+    }
+  }
+  return c;
+};
