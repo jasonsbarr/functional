f279b97fa47d6a19b11802f4c33658e8133f002a
Jason Barr <jason@jasonsbarr.com>
2021-09-17 19:35:49 -0500

    Add safe function, make ifElse return cases take the supplied value

--- src/functions/helpers/ifElse.js
+++ src/functions/helpers/ifElse.js
@@ -1,5 +1,5 @@
 import { curry } from "../lambda/curry.js";
 
 export const ifElse = curry((pred, ifCase, elseCase, value) =>
-  pred(value) ? ifCase : elseCase
+  pred(value) ? ifCase(value) : elseCase(value)
 );
