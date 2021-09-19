764ca4a1a7dcdbb5af57a14b4879626effb868d8
Jason Barr <jason@jasonsbarr.com>
2021-09-18 20:15:21 -0500

    Remove unnecessary curry

--- src/functions/dict/merge.js
+++ src/functions/dict/merge.js
@@ -1,6 +1,4 @@
-import { curry } from "../lambda/curry.js";
 import { extend } from "../object/extend.js";
 
-export const merge = curry((target, ...sources) =>
-  extend(Object.create(null), target, ...sources)
-);
+export const merge = (target, ...sources) =>
+  extend(Object.create(null), target, ...sources);
