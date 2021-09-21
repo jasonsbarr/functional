e80eab03b5ad8c87a58b15a45069fda3409c540f
Jason Barr <jason@jasonsbarr.com>
2021-09-20 21:26:14 -0500

    Make sure to use iter.constructor

--- src/functions/iterable/reduceRight.js
+++ src/functions/iterable/reduceRight.js
@@ -2,5 +2,5 @@ import { curry } from "../lambda/curry.js";
 
 export const reduceRight = curry((fn, initial, iter) => {
   const temp = [...iter];
-  return temp.reduceRight(fn, initial);
+  return iter.constructor(...temp.reduceRight(fn, initial));
 });
