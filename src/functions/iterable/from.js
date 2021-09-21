c4adf96d68dddd9c87c6aaa964f015cf22e15391
Jason Barr <jason@jasonsbarr.com>
2021-09-20 21:20:35 -0500

    Curry from function

--- src/functions/iterable/from.js
+++ src/functions/iterable/from.js
@@ -1,4 +1,5 @@
 import { slice } from "./slice.js";
 import { length } from "./length.js";
+import { curry } from "../lambda/curry.js";
 
-export const from = (i, iter) => slice(iter, i, length(iter) - 1, 1);
+export const from = curry((i, iter) => slice(iter, i, length(iter) - 1, 1));
