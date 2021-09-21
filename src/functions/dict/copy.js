797a5b91387d67c45a30e15220e8bd49b3453e41
Jason Barr <jason.barr@metronetinc.com>
2021-09-21 15:40:42 -0500

    Use internal create function

--- src/functions/dict/copy.js
+++ src/functions/dict/copy.js
@@ -1,10 +1,11 @@
+import { create } from "../object/create.js";
 import { keys } from "../object/keys.js";
 import { symbols } from "../object/symbols.js";
 
 // Makes a shallow copy of a hash object's own keys to a hash object with null prototype.
 // If you want prototype properties or a non-null prototype use object/clone.
 export const copy = (dict) => {
-  let c = Object.create(null);
+  let c = create(null);
   for (let key of keys(dict)) {
     c[key] = dict[key];
   }
