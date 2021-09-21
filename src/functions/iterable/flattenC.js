37ec03d0ac4f9dca16fb3f5b0c74fa25a8042f51
Jason Barr <jason@jasonsbarr.com>
2021-09-20 21:19:52 -0500

    Create curried version of flatten

---
+++ src/functions/iterable/flattenC.js
@@ -0,0 +1,5 @@
+import { curry } from "../lambda/curry.js";
+import { flatten } from "./flatten.js";
+
+// curried version of flatten
+export const flattenC = curry((level, iter) => flatten(iter, level, 0));
