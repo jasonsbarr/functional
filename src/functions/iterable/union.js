9d14066334a077e91c14f87e6b4cb0e366257c05
Jason Barr <jason@jasonsbarr.com>
2021-09-20 21:35:13 -0500

    Curry union function

--- src/functions/iterable/union.js
+++ src/functions/iterable/union.js
@@ -1,8 +1,10 @@
-export const union = (iter1, iter2) => {
+import { curry } from "../lambda/curry.js";
+
+export const union = curry((iter1, iter2) => {
   let set1 = new Set([...iter1]);
   let set2 = new Set([...iter2]);
   for (let item of set2) {
     set1.add(item);
   }
   return iter1.constructor(...set1);
-};
+});
