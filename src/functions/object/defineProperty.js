05a21bcad8029b530dff9ca1453abe7360e274a8
Jason Barr <jason.barr@metronetinc.com>
2021-09-22 15:00:36 -0500

    Fix defineProperty function - extend was already using property function

--- src/functions/object/defineProperty.js
+++ src/functions/object/defineProperty.js
@@ -1,3 +1,2 @@
-import { property } from "./property.js";
 export const defineProperty = (target, key, source) =>
-  Object.defineProperty(target, key, property(source, key));
+  Object.defineProperty(target, key, source);
