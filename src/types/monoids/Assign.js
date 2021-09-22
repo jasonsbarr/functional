61b87dc10f5108203e89bf266cb270937defdf6b
Jason Barr <jason@jasonsbarr.com>
2021-09-21 22:13:12 -0500

    Define Assign monoid

---
+++ src/types/monoids/Assign.js
@@ -0,0 +1,14 @@
+import { assign } from "../../functions/object/assign.js";
+
+export const Assign = (x) => ({
+  kind: "Assign",
+  value: x,
+  concat: ({ value: y }) => assign({}, x, y),
+  inspect: () => `Assign(${x})`,
+  fold: (f) => f(x),
+  map: (f) => Assign(f(x)),
+  chain: (f) => f(x),
+});
+
+Assign.isAssign = (obj) => obj.kind === "Assign";
+Assign.empty = () => ({});
