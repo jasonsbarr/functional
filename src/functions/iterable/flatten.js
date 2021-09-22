cd957233e19325b65b24f5e8547e3262d544601c
Jason Barr <jason.barr@metronetinc.com>
2021-09-22 10:58:01 -0500

    Make sure to actually check against value in isString

--- src/functions/iterable/flatten.js
+++ src/functions/iterable/flatten.js
@@ -7,7 +7,7 @@ import { isString } from "../predicates/isString.js";
 export const flatten = (iter, level = Infinity, current = 0) => {
   let result = [];
   each((item) => {
-    if (isIterable(item) && !isString && current < level) {
+    if (isIterable(item) && !isString(item) && current < level) {
       result = concatToArray(result, flatten(item, level, current + 1));
     } else {
       result.push(item);
