2aeef6d668bbf6cb0120f8844fdf1d4d30425cad
Jason Barr <jason.barr@metronetinc.com>
2021-09-22 09:05:38 -0500

    Add concat case for when dict2 key is nil and not in dict1

--- src/functions/dict/concat.js
+++ src/functions/dict/concat.js
@@ -10,7 +10,7 @@ import { symbols } from "../object/symbols.js";
 // is a concattable type. Uses shallow copy.
 export const concat = curry((dict1, dict2) => {
   let c = copy(dict1);
-  for (let key of [...keys(c), ...symbols(c)]) {
+  for (let key of [...keys(dict2), ...symbols(dict2)]) {
     if (
       typeof c[key] === "string" ||
       typeof c[key] === "number" ||
@@ -20,6 +20,8 @@ export const concat = curry((dict1, dict2) => {
     } else if (typeof c[key] === "boolean") {
       c[key] = c[key] && dict2[key];
     } else if (isNil(c[key])) {
+      c[key] = dict2[key];
+    } else if (isNil(dict2[key])) {
       c[key] = dict2[key];
     } else if (typeof c[key] === "symbol") {
       c[key] = Symbol(c[key].description + dict2[key].description);
