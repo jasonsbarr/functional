f7a7bb44b578ae6c4a9277a917ea22bbae1d8168
Jason Barr <jason.barr@metronetinc.com>
2021-09-22 10:47:45 -0500

    Use isString function instead of checking typeof

--- src/functions/iterable/flatten.js
+++ src/functions/iterable/flatten.js
@@ -1,12 +1,13 @@
 import { each } from "./each.js";
 import { isIterable } from "./isIterable.js";
 import { concatToArray } from "./concatToArray.js";
+import { isString } from "../predicates/isString.js";
 
 // flattens completely or to specified level of depth
 export const flatten = (iter, level = Infinity, current = 0) => {
   let result = [];
   each((item) => {
-    if (isIterable(item) && typeof item !== "string" && current < level) {
+    if (isIterable(item) && !isString && current < level) {
       result = concatToArray(result, flatten(item, level, current + 1));
     } else {
       result.push(item);
