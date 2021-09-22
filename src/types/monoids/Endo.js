d27f05a684a2bba36a54e8e57ac6efff311ed72e
Jason Barr <jason@jasonsbarr.com>
2021-09-21 22:42:54 -0500

    Create Endo monoid

---
+++ src/types/monoids/Endo.js
@@ -0,0 +1,15 @@
+import { identity } from "../../functions/helpers/identity.js";
+
+export const Endo = (x) => ({
+  kind: "Endo",
+  value: x, // must be a function
+  concat: ({ value: y }) => Endo(x(y)),
+  inspect: () => `Endo(${x})`,
+  fold: (f) => f(x),
+  map: (f) => Endo(f(x)),
+  chain: (f) => f(x),
+  run: (v) => x(v),
+});
+
+Endo.isEndo = (obj) => obj.kind === "Endo";
+Endo.empty = () => Endo(identity);
