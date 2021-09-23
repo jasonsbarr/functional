63bfcc7bfe2c696e350246b839ad24296222f6b7
Jason Barr <jason@jasonsbarr.com>
2021-09-22 22:18:41 -0500

    Attempt to create new version of Either class

---
+++ src/types/Either.new.js
@@ -0,0 +1,62 @@
+import { VariantInfo, createType } from "./createType.js";
+import {
+  Alt,
+  Applicative,
+  Apply,
+  Bifunctor,
+  Functor,
+  LeftAlt,
+  LeftApply,
+  LeftBifunctor,
+  LeftClass,
+  LeftFold,
+  LeftFunctor,
+  LeftMonad,
+  LeftSemiGroup,
+  Monad,
+  Monoid,
+  RightClass,
+  RightFold,
+  RightSemiGroup,
+} from "./typeClasses.js";
+
+const variantInfos = [
+  VariantInfo("Right", [
+    RightClass,
+    RightFold,
+    Functor,
+    Apply,
+    Monad,
+    Bifunctor,
+    Alt,
+    RightSemiGroup,
+  ]),
+  VariantInfo("Left", [
+    LeftClass,
+    LeftFold,
+    LeftFunctor,
+    LeftApply,
+    LeftMonad,
+    LeftBifunctor,
+    LeftAlt,
+    LeftSemiGroup,
+  ]),
+];
+
+export const Either = createType(
+  "Either",
+  variantInfos,
+  [Monoid, Applicative],
+  {
+    of(x) {
+      return Either.Right(x);
+    },
+    empty() {
+      return Either.Left(null);
+    },
+  }
+);
+
+const { Left, Right } = Either;
+export { Left };
+export { Right };
