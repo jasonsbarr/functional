c90ddcdb95280bdb81a48fc9247c680bd753b08e
Jason Barr <jason@jasonsbarr.com>
2021-09-22 21:43:15 -0500

    Add the rest of the type classes I've used so far in this project

--- src/types/typeClasses.js
+++ src/types/typeClasses.js
@@ -1,4 +1,16 @@
 // Right typeclasses
+import { concatValues } from "../functions/helpers/concatValues.js";
+
+export const RightClass = {
+  isRight() {
+    return true;
+  },
+
+  isLeft() {
+    return false;
+  },
+};
+
 export const Fold = {
   fold(f) {
     return f(this.value);
@@ -23,7 +35,54 @@ export const Apply = {
   },
 };
 
+export const Monad = {
+  chain(f) {
+    return f(this.value);
+  },
+};
+
+export const Bifunctor = {
+  bimap(leftFunc, rightFunc) {
+    return this.constructor(rightFunc(this.value));
+  },
+
+  bichain(leftFunc, rightFunc) {
+    return rightFunc(this.value);
+  },
+};
+
+export const Alt = {
+  alt(other) {
+    return this;
+  },
+};
+
+export const SemiGroup = {
+  concat(o) {
+    return this.constructor(concatValues(this.value, o.value));
+  },
+};
+
+export const RightSemiGroup = {
+  concat(o) {
+    return o.fold(
+      (l) => o.constructor(l),
+      (r) => this.constructor(concatValues(this.value, r))
+    );
+  },
+};
+
 // Left typeclasses
+export const LeftClass = {
+  isRight() {
+    return false;
+  },
+
+  isLeft() {
+    return true;
+  },
+};
+
 export const LeftFold = {
   fold(f, g) {
     return f(this.value);
@@ -32,7 +91,41 @@ export const LeftFold = {
 
 export const LeftFunctor = {
   map(f) {
-    return this.value;
+    return this;
+  },
+};
+
+export const LeftApply = {
+  ap(other) {
+    return this;
+  },
+};
+
+export const LeftMonad = {
+  chain(f) {
+    return this;
+  },
+};
+
+export const LeftBifunctor = {
+  bimap(leftFunc, rightFunc) {
+    return this.constructor(leftFunc(this.value));
+  },
+
+  bichain(leftFunc, rightFunc) {
+    return leftFunc(this.value);
+  },
+};
+
+export const LeftAlt = {
+  alt(other) {
+    return other.isRight() ? other : this;
+  },
+};
+
+export const LeftSemiGroup = {
+  concat(o) {
+    return this;
   },
 };
 
@@ -40,5 +133,13 @@ export const LeftFunctor = {
 export const Applicative = {
   of() {
     throw new Error("of method must be implemented individually for each type");
+  },
+};
+
+export const Monoid = {
+  empty() {
+    throw new Error(
+      "empty method must be implemented individually for each type"
+    );
   },
 };
