744c20c083a108cec9c5f3d6b12ee3133a52aae9
Jason Barr <jason@jasonsbarr.com>
2021-09-24 18:56:09 -0500

    Update imports

--- packages/types/monoids/Sum.js
+++ packages/types/monoids/Sum.js
@@ -1,7 +1,15 @@
-import { VariantInfo, createType } from "../../functions/type/createType.js";
-import { Fold, Monoid, Setoid, SemiGroup } from "../typeClasses.js";
-import { isFunction } from "../../functions/predicates/isFunction.js";
-import { isNumber } from "../../functions/predicates/isNumber.js";
+import {
+  VariantInfo,
+  createType,
+} from "@jasonsbarr/functional-core/functions/type/createType.js";
+import {
+  Fold,
+  Monoid,
+  Setoid,
+  SemiGroup,
+} from "@jasonsbarr/functional-core/types/typeClasses.js";
+import { isFunction } from "@jasonsbarr/functional-core/functions/predicates/isFunction.js";
+import { isNumber } from "@jasonsbarr/functional-core/functions/predicates/isNumber.js";
 
 const variantInfos = [
   VariantInfo(
