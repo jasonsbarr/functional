277b7bd0200598c26fc2d9f153582412bfdff70e
Jason Barr <jason@jasonsbarr.com>
2021-09-23 20:09:52 -0500

    Re-create Sum monoid using new type constructor

--- src/types/Sum.js
+++ src/types/Sum.js
@@ -1,14 +1,39 @@
-// use for adding numbers or concatting strings
-export const Sum = (x) => ({
-  kind: "Sum",
-  value: x,
-  concat: ({ value: y }) => Sum(x + y),
-  inspect: () => `Sum(${x})`,
-  fold: (f) => f(x),
-  map: (f) => Sum(f(x)),
-  ap: (o) => o.map(x),
-  chain: (f) => f(x),
-});
+import { VariantInfo, createType } from "./createType.js";
+import { Fold, Monoid, Setoid, SemiGroup } from "./typeClasses.js";
+import { isFunction } from "../functions/predicates/isFunction.js";
+import { isNumber } from "../functions/predicates/isNumber.js";
+import { number } from "../functions/number/number.js";
 
-Sum.isSum = (obj) => obj.kind === "Sum";
-Sum.empty = () => Sum(0);
+const variantInfos = [
+  VariantInfo(
+    "Sum",
+    [Fold, SemiGroup, Setoid],
+    {
+      concat({ value: y }) {
+        return Sum(this.value + y);
+      },
+
+      inspect() {
+        return `Sum(${this.value})`;
+      },
+
+      init() {
+        this.value = isNumber(this.value) ? this.value : number(this.value);
+      },
+    },
+    {
+      sTypeClasses: [Monoid],
+      methods: {
+        empty() {
+          return Sum(0);
+        },
+
+        isSum(x) {
+          return x && isFunction(x.isSum) && x.isSum();
+        },
+      },
+    }
+  ),
+];
+
+export const { Sum } = createType("Sum", variantInfos);
