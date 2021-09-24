15c4aea01c2b1f0d1bb72b29a67f8892f51858cc
Jason Barr <jason@jasonsbarr.com>
2021-09-24 17:31:34 -0500

    Delete all index.js files because we're going to make them import things individually

--- packages/core/functions/index.js
+++
@@ -1,6 +0,0 @@
-import { Dict } from "./dict/index.js";
-import { Helpers } from "./helpers/index.js";
-import { Iterable } from "./iterable/index.js";
-import { Lambda } from "./lambda/index.js";
-
-export default { Dict, Helpers, Iterable, Lambda };
--- packages/core/index.js
+++
@@ -1,6 +0,0 @@
-import Functions from "./functions/index.js";
-import Types from "./types/index.js";
-
-export default { ...Functions, ...Types };
-export const Functions = { ...Functions };
-export const Types = { ...Types };
