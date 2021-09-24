a432b9f76589eeb265b2425f83c19f57c9d8fdb6
Jason Barr <jason.barr@metronetinc.com>
2021-09-24 10:26:30 -0500

    Edit includes to allow searching via function or regexp

--- src/functions/iterable/includes.js
+++ src/functions/iterable/includes.js
@@ -1,11 +1,17 @@
 import { curry } from "../lambda/curry.js";
 import { equals } from "../object/equals.js";
+import { isFunction } from "../predicates/isFunction.js";
+import { isRegExp } from "../predicates/isRegExp.js";
 
 // works with any value, including objects
-export const includes = curry((value, iter) => {
+export const includes = curry((search, iter) => {
   for (let item of iter) {
-    if (equals(item, value)) {
-      return true;
+    if (isFunction(search)) {
+      if (search(item)) return true;
+    } else if (isRegExp(search)) {
+      if (search.test(item)) return true;
+    } else {
+      if (equals(item, search)) return true;
     }
   }
   return false;
