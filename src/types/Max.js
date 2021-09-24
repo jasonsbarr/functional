e4a0fcd9706fb4d245eb3b4ae656ca0ccbdeb6f7
Jason Barr <jason@jasonsbarr.com>
2021-09-23 19:03:58 -0500

    Re-create Max monoid using new type constructor

--- src/types/Max.js
+++ src/types/Max.js
@@ -1,13 +1,40 @@
-export const Max = (x) => ({
-  kind: "Max",
-  value: x,
-  concat: ({ value: y }) => Max(x > y ? x : y),
-  inspect: () => `Max(${x})`,
-  fold: (f) => f(x),
-  map: (f) => Max(f(x)),
-  ap: (o) => o.map(x),
-  chain: (f) => f(x),
-});
+import { VariantInfo, createType } from "./createType.js";
+import { Fold, Monoid, Setoid, SemiGroup } from "./typeClasses.js";
+import { isFunction } from "../functions/predicates/isFunction.js";
+import { isNumber } from "../functions/predicates/isNumber.js";
+import { number } from "../functions/number/number.js";
+import { max } from "../functions/math/max.js";
 
-Max.isMax = (obj) => obj.kind === "Max";
-Max.empty = () => Max(-Infinity);
+const variantInfos = [
+  VariantInfo(
+    "Max",
+    [Fold, SemiGroup, Setoid],
+    {
+      concat({ value: y }) {
+        return Max(max(this.value, y));
+      },
+
+      inspect() {
+        return `Max(${this.value})`;
+      },
+
+      init() {
+        this.value = isNumber(this.value) ? this.value : number(this.value);
+      },
+    },
+    {
+      sTypeClasses: [Monoid],
+      methods: {
+        empty() {
+          return Max(-Infinity);
+        },
+
+        isMax(x) {
+          return x && isFunction(x.isMax) && x.isMax();
+        },
+      },
+    }
+  ),
+];
+
+export const { Max } = createType("Max", variantInfos);
