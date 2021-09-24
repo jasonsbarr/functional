766378474422c3e0559ccbff932d174c185ecfa4
Jason Barr <jason@jasonsbarr.com>
2021-09-23 19:55:02 -0500

    Re-implement Product using new type constructor

--- src/types/Product.js
+++ src/types/Product.js
@@ -1,12 +1,39 @@
-export const Product = (x) => ({
-  kind: "Product",
-  value: x,
-  concat: ({ value: y }) => Product(x * y),
-  inspect: () => `Product(${x})`,
-  fold: (f) => f(x),
-  map: (f) => Product(f(x)),
-  chain: (f) => f(x),
-});
+import { VariantInfo, createType } from "./createType.js";
+import { Fold, Monoid, SemiGroup, Setoid } from "./typeClasses.js";
+import { isFunction } from "../functions/predicates/isFunction.js";
+import { isNumber } from "../functions/predicates/isNumber.js";
+import { number } from "../functions/number/number.js";
 
-Product.isProduct = (obj) => obj.kind === "Product";
-Product.empty = () => Product(1);
+const variantInfos = [
+  VariantInfo(
+    "Product",
+    [Fold, SemiGroup, Setoid],
+    {
+      concat({ value: y }) {
+        return Product(this.value * y);
+      },
+
+      inspect() {
+        return `Product(${this.value})`;
+      },
+
+      init() {
+        this.value = isNumber(this.value) ? this.value : number(this.value);
+      },
+    },
+    {
+      sTypeClasses: [Monoid],
+      methods: {
+        empty() {
+          return Product(1);
+        },
+
+        isProduct(x) {
+          return x && isFunction(x.isProduct) && x.isProduct();
+        },
+      },
+    }
+  ),
+];
+
+export const { Product } = createType("Product", variantInfos);
