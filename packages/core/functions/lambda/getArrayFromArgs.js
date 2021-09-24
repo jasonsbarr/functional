7e93a5becb6682634834f33c8e899a18942b67b7
Jason Barr <jason@jasonsbarr.com>
2021-09-24 17:28:49 -0500

    Use isString function

--- packages/core/functions/lambda/getArrayFromArgs.js
+++ packages/core/functions/lambda/getArrayFromArgs.js
@@ -1,5 +1,6 @@
 import { isIterable } from "../predicates/isIterable.js";
+import { isString } from "../predicates/isString.js";
 
 // Returns a single array of the function args based on whether the first arg is an iterable
 export const getArrayFromArgs = (...args) =>
-  isIterable(args[0]) && typeof args[0] !== "string" ? [...args[0]] : args;
+  isIterable(args[0]) && !isString(args[0]) ? [...args[0]] : args;
