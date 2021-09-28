2a42bdeaf30d806d99f45af437e63c88ab0c0b65
Jason Barr <jason@jasonsbarr.com>
2021-09-28 09:06:43 -0500

    Use length function

--- packages/core/lib/object/toQueryString.js
+++ packages/core/lib/object/toQueryString.js
@@ -1,12 +1,13 @@
 import { entries } from "./entries.js";
 import { lt } from "../predicates/lt.js";
+import { length } from "../array/length.js";
 
 export const toQueryString = (dict) => {
   let qStr = "";
   let i = 0;
   for (let [k, v] of entries(dict)) {
     qStr += encodeURI(k) + "=" + encodeURI(v);
-    if (lt(i, entries(dict).length - 1)) {
+    if (lt(i, length(entries(dict)) - 1)) {
       qStr += "&";
     }
     i++;
