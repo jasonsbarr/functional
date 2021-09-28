3f88e268716a2933edf2311e823a8b3f55fd3b0a
Jason Barr <jason.barr@metronetinc.com>
2021-09-28 10:07:39 -0500

    Strings and Arrays are SemiGroups too

--- packages/core/lib/predicates/isSemiGroup.js
+++ packages/core/lib/predicates/isSemiGroup.js
@@ -1,3 +1,6 @@
 import { isFunction } from "./isFunction.js";
+import { isString } from "../predicates/isString.js";
+import { isArray } from "../predicates/isArray.js";
 
-export const isSemiGroup = (obj) => isFunction(obj.concat);
+export const isSemiGroup = (obj) =>
+  isFunction(obj.concat) || isString(obj) || isArray(obj);
