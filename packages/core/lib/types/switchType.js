3d744436503c117860b75b23002ceee513af7772
Jason Barr <jason@jasonsbarr.com>
2021-10-31 21:02:04 -0500

    Enable using _ as catchall that disables exhaustiveness checking

--- packages/core/lib/types/switchType.js
+++ packages/core/lib/types/switchType.js
@@ -1,6 +1,7 @@
 import { keys } from "../object/keys.js";
 import { includes } from "../array/includes.js";
 import { assert } from "..helpers/assert.js";
+import { not } from "../helpers/not.js";
 import { curry } from "../lambda/curry.js";
 /**
  *
@@ -11,17 +12,19 @@ import { curry } from "../lambda/curry.js";
  */
 export const switchType = curry((typeRepresentative, dispatcher, instance) => {
   const cases = keys(dispatcher);
-  for (let variant of typeRepresentative.variants) {
-    assert(
-      includes(variant, cases),
-      "switchType must take a case for every variant of a type"
-    );
-  }
-  for (let typeCase of cases) {
-    assert(
-      includes(typeCase, typeRepresentative.variants),
-      "Variant not found for switch case"
-    );
+  if (not(includes("_", cases))) {
+    for (let variant of typeRepresentative.variants) {
+      assert(
+        includes(variant, cases),
+        "switchType must take a case for every variant of a type"
+      );
+    }
+    for (let typeCase of cases) {
+      assert(
+        includes(typeCase, typeRepresentative.variants),
+        "Variant not found for switch case"
+      );
+    }
   }
   return dispatcher[instance.variant](instance);
 });
