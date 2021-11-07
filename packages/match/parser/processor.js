00e903ce315e6e8dd24b21080795a724af439f18
Jason Barr <jason@jasonsbarr.com>
2021-11-07 01:05:28 -0500

    Export function

--- packages/match/parser/processor.js
+++ packages/match/parser/processor.js
@@ -1,6 +1,6 @@
 import parse from "./parser.js";
 
-const split = (code) => {
+export default (code) => {
   const split1 = code.split("->");
   const [pattern, when] = split1[0].split("when");
   const expr = split1[1];
