932ea197f558764f710e4f8b789b54a5340ed80c
Jason Barr <jason@jasonsbarr.com>
2021-11-06 22:30:37 -0500

    Stub out parser

--- packages/match/parser/parser.js
+++ packages/match/parser/parser.js
@@ -1,2 +1,8 @@
 import { pipeline } from "@jasonsbarr/functional-core/lib/lambda/pipeline.js";
 import read from "./lexer.js";
+
+const raw = (str) => String.raw`${str}`;
+
+const parse = (input) => {};
+
+export default (code) => pipeline(code, raw, read, parse);
