dc3aec8cbbf07197d022a0f9aa55c68222075af1
Jason Barr <jason@jasonsbarr.com>
2021-11-06 22:55:50 -0500

    Add literal nodes

--- packages/match/parser/nodes.js
+++ packages/match/parser/nodes.js
@@ -1,4 +1,7 @@
 // AST nodes for pattern matching DSL, which includes arbitrary JavaScript expressions
+// JavaScript nodes should roughly match the Babel spec
+// https://github.com/babel/babel/blob/main/packages/babel-parser/ast/spec.md
+
 // The whole pattern expression, including optional when clause
 export const PatternExpression = (pattern, when, expr) => ({
   type: "PatternExpression",
@@ -9,5 +12,11 @@ export const PatternExpression = (pattern, when, expr) => ({
 
 // Literals
 export const NumericLiteral = (value) => ({ type: "NumericLiteral", value });
+export const BigIntLiteral = (value) => ({ type: "BigIntLiteral", value });
 export const StringLiteral = (value) => ({ type: "StringLiteral", value });
+export const BooleanLiteral = (value) => ({ type: "BooleanLiteral", value });
 export const NullLiteral = () => ({ type: "NullLiteral", value: null });
+export const UndefinedLiteral = () => ({
+  type: "UndefinedLiteral",
+  value: undefined,
+});
