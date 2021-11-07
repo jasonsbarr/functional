8e25867d39e929b1acbb6e26f75189453fe08ba2
Jason Barr <jason@jasonsbarr.com>
2021-11-06 22:55:14 -0500

    Add parser helper functions

--- packages/match/parser/parser.js
+++ packages/match/parser/parser.js
@@ -3,6 +3,28 @@ import read from "./lexer.js";
 
 const raw = (str) => String.raw`${str}`;
 
-const parse = (input) => {};
+const parse = (whichCase) => (input) => {
+  let mode = "pattern";
+  let context = "statement";
+  let pos = 0;
 
-export default (code) => pipeline(code, raw, read, parse);
+  const peek = () => input[pos];
+  const next = () => input[++pos];
+  const lookahead = (i) => input[pos + i];
+  const skip = () => pos++;
+  const eoi = () => input[pos] === undefined;
+  const croak = (msg) => {
+    throw new SyntaxError(msg);
+  };
+  const skipIf = (pred, expected) => {
+    const tok = peek();
+    pred(peek())
+      ? skip()
+      : croak(
+          `Invalid token: expected ${expected}, got ${tok.value} at pos ${tok.pos} in case ${whichCase}`
+        );
+  };
+};
+
+export default (code, whichCase) =>
+  pipeline(code, raw, read(whichCase), parse(whichCase));
