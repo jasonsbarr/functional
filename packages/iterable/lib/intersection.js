671799915edc75de6a8e79550eb574252d7681ae
Jason Barr <jason@jasonsbarr.com>
2021-11-08 22:05:42 -0600

    Re-implement intersection function using check by structural equality

--- packages/iterable/lib/intersection.js
+++ packages/iterable/lib/intersection.js
@@ -1,13 +1,17 @@
 import { curry } from "@jasonsbarr/functional-core/lib/lambda/curry.js";
+import { includes } from "./includes.js";
+import { unique } from "./unique.js";
 
 export const intersection = curry((iter1, iter2) => {
-  let set1 = new Set([...iter1]);
-  let set2 = new Set([...iter2]);
+  let set1 = unique(iter1);
+  let set2 = unique(iter2);
   let result = [];
+
   for (let item of set2) {
-    if (set1.has(item)) {
+    if (includes(item, set1)) {
       result.push(item);
     }
   }
+
   return iter1.constructor(...result);
 });
