33dcacfcf22ef9b026780169358d3be800697181
Jason Barr <jason@jasonsbarr.com>
2021-11-08 21:50:46 -0600

    Rewrite unique function to work with object structural equality

--- packages/iterable/lib/unique.js
+++ packages/iterable/lib/unique.js
@@ -1,1 +1,19 @@
-export const unique = (iter) => iter.constructor(...[...new Set([...iter])]);
+import { equals } from "@jasonsbarr/functional-core/lib/object/equals.js";
+
+export const unique = (iter) => {
+  let temp = [];
+
+  for (let item of iter) {
+    let uniq = true;
+    for (let i of temp) {
+      if (equals(i, item)) {
+        uniq = false;
+      }
+    }
+    if (uniq) {
+      temp.push(item);
+    }
+  }
+
+  return iter.constructor(...temp);
+};
