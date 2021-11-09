4e70ab1488d8c677f4fddb76da93daca9dfcbf51
Jason Barr <jason@jasonsbarr.com>
2021-11-08 22:02:39 -0600

    Rewrite symmetric difference function to check by structural equality

--- packages/iterable/lib/symmetricDifference.js
+++ packages/iterable/lib/symmetricDifference.js
@@ -1,14 +1,20 @@
 import { curry } from "@jasonsbarr/functional-core/lib/lambda/curry.js";
+import { unique } from "./unique.js";
+import { includes } from "./includes.js";
+import { remove } from "./remove.js";
+import { append } from "./append.js";
 
 export const symmetricDifference = curry((iter1, iter2) => {
-  let set1 = new Set([...iter1]);
-  let set2 = new Set([...iter2]);
+  let set1 = unique(iter1);
+  let set2 = unique(iter2);
+
   for (let item of set2) {
-    if (set1.has(item)) {
-      set1.delete(item);
+    if (includes(item, set1)) {
+      set1 = remove(item, set1);
     } else {
-      set1.add(item);
+      set1 = append(item, set1);
     }
   }
-  return iter1.constructor(...set1);
+
+  return unique(set1);
 });
