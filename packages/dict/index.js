fa30420988e3d467b472331ab87b73b53b415d49
Jason Barr <jason@jasonsbarr.com>
2021-11-24 20:43:01 -0600

    Export dict functions from index

---
+++ packages/dict/index.js
@@ -0,0 +1,93 @@
+import { all } from "./lib/all.js";
+import { any } from "./lib/any.js";
+import { append } from "./lib/append.js";
+import { clear } from "./lib/clear.js";
+import { compact } from "./lib/compact.js";
+import { concat } from "./lib/compact.js";
+import { copy } from "./lib/copy.js";
+import { count } from "./lib/count.js";
+import { deleteValue } from "./lib/deleteValue.js";
+import { each } from "./lib/each.js";
+import { eachWithKey } from "./lib/eachWithKey.js";
+import { filter } from "./lib/filter.js";
+import { filterKeys } from "./lib/filterKeys.js";
+import { find } from "./lib/find.js";
+import { findEntry } from "./lib/findEntry.js";
+import { findKey } from "./lib/findKey.js";
+import { first } from "./lib/first.js";
+import { fold } from "./lib/fold.js";
+import { foldLeft } from "./lib/foldLeft.js";
+import { foldRight } from "./lib/foldRight.js";
+import { get } from "./lib/get.js";
+import { getWithDefault } from "./lib/getWithDefault.js";
+import { has } from "./lib/has.js";
+import { index } from "./lib/index.js";
+import { join } from "./lib/join.js";
+import { last } from "./lib/last.js";
+import { map } from "./lib/map.js";
+import { mapEntries } from "./lib/mapEntries.js";
+import { mapKeys } from "./lib/mapKeys.js";
+import { merge } from "./lib/merge.js";
+import { none } from "./lib/none.js";
+import { pluck } from "./lib/pluck.js";
+import { prepend } from "./lib/prepend.js";
+import { reduce } from "./lib/reduce.js";
+import { reduceRight } from "./lib/reduceRight.js";
+import { reject } from "./lib/reject.js";
+import { removeByKey } from "./lib/removeByKey.js";
+import { removeByValue } from "./lib/removeByValue.js";
+import { reverse } from "./lib/reverse.js";
+import { set } from "./lib/set.js";
+import { sort } from "./lib/sort.js";
+import { toMap } from "./lib/toMap.js";
+import { toObject } from "./lib/toObject.js";
+import { unset } from "./lib/unset.js";
+import { update } from "./lib/update.js";
+
+export {
+  all,
+  any,
+  append,
+  clear,
+  compact,
+  concat,
+  copy,
+  count,
+  deleteValue,
+  each,
+  eachWithKey,
+  filter,
+  filterKeys,
+  find,
+  findEntry,
+  findKey,
+  first,
+  fold,
+  foldLeft,
+  foldRight,
+  get,
+  getWithDefault,
+  has,
+  index,
+  join,
+  last,
+  map,
+  mapEntries,
+  mapKeys,
+  merge,
+  none,
+  pluck,
+  prepend,
+  reduce,
+  reduceRight,
+  reject,
+  removeByKey,
+  removeByValue,
+  reverse,
+  set,
+  sort,
+  toMap,
+  toObject,
+  unset,
+  update,
+};
