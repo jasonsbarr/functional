42e82ac161382df19f9e50e4965a2e19ea3d7faa
Jason Barr <jason@jasonsbarr.com>
2021-11-24 20:07:13 -0600

    Export types package types from index file

---
+++ packages/types/lib/index.js
@@ -0,0 +1,35 @@
+import { All } from "./All.js";
+import { Any } from "./Any.js";
+import { Assign } from "./Assign.js";
+import { Effect } from "./Effect.js";
+import { Endo } from "./Endo.js";
+import { First } from "./First.js";
+import { Iso } from "./Iso.js";
+import { Last } from "./Last.js";
+import { Max } from "./Max.js";
+import { Min } from "./Min.js";
+import { Pair } from "./Pair.js";
+import { Product } from "./Product.js";
+import { Sum } from "./Sum.js";
+import { Validation, Success, Failure, succeed, fail } from "./Validation.js";
+
+export {
+  All,
+  Any,
+  Assign,
+  Effect,
+  Endo,
+  Failure,
+  fail,
+  First,
+  Iso,
+  Last,
+  Max,
+  Min,
+  Pair,
+  Product,
+  Success,
+  succeed,
+  Sum,
+  Validation,
+};
