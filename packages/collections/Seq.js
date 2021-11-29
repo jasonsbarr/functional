af9f5e8718760fbd5ae7d4508874fd1c4b3b282d
Jason Barr <jason.barr@metronetinc.com>
2021-11-29 15:14:35 -0600

    Extend Sequence to Array types

--- packages/collections/Seq.js
+++ packages/collections/Seq.js
@@ -1,14 +1,45 @@
+import { isArray } from "@jasonsbarr/functional-core/lib/predicates/isArray.js";
+
 class Sequence {
   constructor(source) {
     this.source = source;
   }
 }
 
-class MappedSequence extends Sequence {}
+class MappedSequence extends Sequence {
+  constructor(parent, mapFn) {
+    super(parent.source);
+    this.parent = parent;
+    this.mapFn = mapFn;
+  }
+}
 
-class FilteredSequence extends Sequence {}
+class FilteredSequence extends Sequence {
+  constructor(parent, filterFn) {
+    super(parent.source);
+    this.parent = parent;
+    this.filterFn = filterFn;
+  }
+}
 
-export const Seq = (iter) => new Sequence(iter);
+class ArrayLikeSequence extends Sequence {
+  constructor(source) {
+    super(source);
+  }
+}
+
+class ArrayWrapper extends ArrayLikeSequence {
+  constructor(source) {
+    super(source);
+  }
+}
+
+export const Seq = (iter) =>
+  isArray(iter)
+    ? new ArrayWrapper(iter)
+    : Seq.isSeq(iter)
+    ? iter
+    : new Sequence(iter);
 
 Seq.of = (iter) => Seq(iter);
 Seq.from = Seq.of;
