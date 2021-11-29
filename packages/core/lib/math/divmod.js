cefb5d2f3025bfa997924fe524779fc3f2c28a49
Jason Barr <jason.barr@metronetinc.com>
2021-11-29 14:46:02 -0600

    Reverse order of arguments in divmod

--- packages/core/lib/math/divmod.js
+++ packages/core/lib/math/divmod.js
@@ -1,7 +1,7 @@
 import { curry } from "../lambda/curry.js";
 import { eq } from "../predicates/eq.js";
 
-export const divmod = curry((dividend, divisor) => {
+export const divmod = curry((divisor, dividend) => {
   if (eq(divisor, 0)) {
     throw new Error("Cannot divide by 0");
   }
