1ebe429e0efeeeddcaec88b8a331bbd5629360dd
Jason Barr <jason.barr@metronetinc.com>
2021-11-29 14:40:00 -0600

    Pluck properties on an object, so take and pluck aren't the same now

--- packages/iterable/lib/pluck.js
+++ packages/iterable/lib/pluck.js
@@ -1,4 +1,18 @@
 import { curry } from "@jasonsbarr/functional-core/lib/lambda/curry.js";
-import { slice } from "./slice.js";
+import { prop } from "@jasonsbarr/functional-core/lib/object/prop.js";
+import { Option } from "@jasonsbarr/functional-core";
+import { map } from "./map.js";
+import { reject } from "./reject.js";
+import { sequence } from "./sequence.js";
 
-export const pluck = curry((numItems, iter) => slice(iter, 0, numItems, 1));
+// prop returns an Option, so we reject all None values, sequence the result
+// into an Option of the mapped iterable, then fold into the final iterable
+export const pluck = curry((p, iter) =>
+  sequence(
+    Option.of,
+    reject((el) => Option.isNone(el), map(prop(p), iter))
+  ).fold(
+    () => iter.constructor(...[]),
+    (x) => x
+  )
+);
