43148a488548903cdeb4f614ceff695d8b8eb791
Jason Barr <jason.barr@metronet.com>
2023-07-06 08:47:47 -0500

    Don't need create function if it's a base object

--- packages/core/lib/types/createType.js
+++ packages/core/lib/types/createType.js
@@ -85,7 +85,7 @@ const createVariantConstructor = (
         value: args.length === 1 ? args[0] : args,
       });
     } else {
-      let obj = create({});
+      let obj = {};
       let i = 0;
 
       for (let field of fields) {
