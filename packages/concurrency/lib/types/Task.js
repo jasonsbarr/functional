c9d5d376b79cb66ab0b2ac605ce46b838dc82bb8
Jason Barr <jason.barr@metronet.com>
2023-07-06 10:10:38 -0500

    First attempt at run method

--- packages/concurrency/lib/types/Task.js
+++ packages/concurrency/lib/types/Task.js
@@ -1,3 +1,6 @@
+import { noop } from "@jasonsbarr/functional-core";
+import { Deferred } from "../internal/_deferred.js";
+import { TaskExecution } from "./TaskExecution.js";
 /**
  * @callback Reject
  * @param {any} reason
@@ -28,11 +31,46 @@ export class TaskClass {
   /**
    * Task constructor
    * @param {Computation} computation
-   * @param {Cleanup} cleanup
+   * @param {Cleanup} [cleanup=() => {}]
    */
-  constructor(computation, cleanup) {
+  constructor(computation, cleanup = noop) {
     this._computation = computation;
     this._cleanup = cleanup;
+  }
+
+  run() {
+    let deferred = new Deferred();
+    let cleanups = [];
+    let computation = this._computation;
+    let cleanup = this._cleanup;
+
+    deferred.listen({
+      onCancelled: () => {
+        cleanups.forEach((f) => f());
+        cleanups = [];
+      },
+
+      onResolved: (_value) => {
+        cleanups.forEach((f) => f());
+        cleanups = [];
+      },
+
+      onRejected: (_reason) => {
+        cleanups.forEach((f) => f());
+        cleanups = [];
+      },
+    });
+
+    const execution = new TaskExecution(this, deferred);
+
+    computation(
+      (reason) => deferred.reject(reason),
+      (value) => deferred.resolve(value),
+      () => deferred.maybeCancel()
+    );
+    cleanups.push(cleanup);
+
+    return execution;
   }
 }
 
