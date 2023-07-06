524dacd1ca2b28213de74b3c6b4760173572d097
Jason Barr <jason.barr@metronet.com>
2023-07-06 08:26:03 -0500

    apRejected doesn't make sense

--- packages/concurrency/lib/types/Future.js
+++ packages/concurrency/lib/types/Future.js
@@ -125,10 +125,6 @@ export class Future extends Deferred {
     return this.chain((f) => future.map(f));
   }
 
-  apRejected(future) {
-    return this.chain((f) => future.mapRejected(f));
-  }
-
   finalize(pred, value, reason = null) {
     return pred(value) ? this.resolve(value) : this.reject(reason ?? value);
   }
